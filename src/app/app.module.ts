e0f6300b1a66b5dfc3b2b1254124b4ec69abf500
marudits <marudi@coss.io>
2019-03-17 22:43:10 +0800

    :construction: WIP | Add element-angular as UI Library and create header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
+
+// libraries
+import { ElModule } from 'element-angular/release/element-angular.module';
 
 // modules
 import { AppRoutingModule } from '../modules/app-routing/app-routing.module';
@@ -10,10 +16,14 @@ import { AppRoutingModule } from '../modules/app-routing/app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ElModule.forRoot()
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
